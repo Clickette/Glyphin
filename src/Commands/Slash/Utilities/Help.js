d45da8cc1cdbd38ae1f3fbe85b8069d6b2dbe5e3
ClaytonTDM <claytontdm@gmail.com>
2024-06-04 17:01:14 -0400

    capitalized

--- src/Commands/Slash/Utilities/Help.js
+++ src/Commands/Slash/Utilities/Help.js
@@ -30,11 +30,11 @@ module.exports = {
             }
 
             // Create an embed message using EmbedBuilder
-            const embed = new EmbedBuilder();
-            embed.setColor('F8C923');
-            embed.setTitle('Help');
-            embed.setDescription(helpMessage); // Set the description to the help message
-            embed.setImage('https://media.discordapp.net/attachments/1247588047831437435/1247641355933581322/glyphin_embed_small.png?ex=6660c40f&is=665f728f&hm=6d2acb0014177fe245b90289be05e5dccf6aa48a299f66923079f04b0102c77f&format=webp&quality=lossless&');
+            const embed = new EmbedBuilder()
+                .setColor('F8C923')
+                .setTitle('Help')
+                .setDescription(helpMessage) // Set the description to the help message
+                .setImage("https://media.discordapp.net/attachments/1247588047831437435/1247641355933581322/glyphin_embed_small.png?ex=6660c40f&is=665f728f&hm=6d2acb0014177fe245b90289be05e5dccf6aa48a299f66923079f04b0102c77f&=&format=webp&quality=lossless");
 
             // Send the embed message
             interaction.reply({ embeds: [embed] });
