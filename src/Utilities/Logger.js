81492d6562cc1c6ec8310ee64e162eb5918fb9f9
unium <81354905+TheUnium@users.noreply.github.com>
2024-06-06 10:23:41 +0530

    added function to log json data, jsdocs

--- src/Utilities/Logger.js
+++ src/Utilities/Logger.js
@@ -2,17 +2,50 @@ const fncy = require('fancy-time');
 const chalk = require('chalk');
 
 const Logger = {
+  /**
+   * Logs an informational message.
+   *
+   * @param {string} message - The message to log.
+   */
   info: function(message) {
     console.log(chalk.blue(fncy.timeStr(`[INFO] ${message}`)));
   },
+
+  /**
+   * Logs a warning message.
+   *
+   * @param {string} message - The message to log.
+   */
   warn: function(message) {
     console.log(chalk.yellow(fncy.timeStr(`[WARN] ${message}`)));
   },
+
+  /**
+   * Logs an error message.
+   *
+   * @param {string} message - The message to log.
+   */
   error: function(message) {
     console.log(chalk.red(fncy.timeStr(`[ERROR] ${message}`)));
   },
+
+  /**
+   * Logs a standard message.
+   *
+   * @param {string} message - The message to log.
+   */
   std: function(message) {
     console.log(chalk.white(fncy.timeStr(message)));
+  },
+
+  /**
+   * Logs JSON content.
+   *
+   * @param {Object} json - The JSON object to log.
+   */
+  json: function(json) {
+    const formattedJson = JSON.stringify(json, null, 2); // Pretty print JSON
+    console.log(chalk.green(fncy.timeStr(`[JSON] ${formattedJson}`)));
   }
 };
 
