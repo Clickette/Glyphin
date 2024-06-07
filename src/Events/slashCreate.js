cd201cc00f7c99d00dfa147f45e999a0d76b6bae
unium <81354905+TheUnium@users.noreply.github.com>
2024-06-07 10:45:28 +0530

    module aliases

--- src/Events/slashCreate.js
+++ src/Events/slashCreate.js
@@ -1,21 +1,21 @@
 const { Events } = require("discord.js");
-const Logger = require('../Utilities/Logger.js');
+const Logger = require('@utils/Logger');
 
 module.exports = {
-	name: Events.InteractionCreate,
-	async execute(interaction) {
-		const { client } = interaction;
-		if (!interaction.isChatInputCommand()) return;
-		const command = client.slashCommands.get(interaction.commandName);
-		if (!command) return;
-		try {
-			await command.execute(interaction);
-		} catch (error) {
-			Logger.error(error);
-			await interaction.reply({
-				content: "An error occured while attempting to execute this command! Please report this to the developers.",
-				ephemeral: true,
-			});
-		}
-	},
+    name: Events.InteractionCreate,
+    async execute(interaction) {
+        const { client } = interaction;
+        if (!interaction.isChatInputCommand()) return;
+        const command = client.slashCommands.get(interaction.commandName);
+        if (!command) return;
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            Logger.error(error);
+            await interaction.reply({
+                content: "An error occurred while attempting to execute this command! Please report this to the developers.",
+                ephemeral: true,
+            });
+        }
+    },
 };
