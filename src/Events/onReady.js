305c64b06c3c941d8f4a1647762987e7d458bb84
hamhim <hamhimstudiologin@gmail.com>
2024-06-08 20:21:27 +0530

    status

--- src/Events/onReady.js
+++ src/Events/onReady.js
@@ -1,10 +1,15 @@
-const { Events } = require("discord.js");
+const { Events, ActivityType } = require("discord.js");
 const Logger = require('@utils/Logger');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
-		Logger.info(`Hello World! Logged in as user : ${client.user.tag}`);
+		Logger.info(`We're online! Logged in as user : ${client.user.tag}`);
+		
+		client.user.setPresence({
+			activities: [{ name: '/help', type: ActivityType.Playing }],
+			status: 'online',
+		});
 	},
 };
